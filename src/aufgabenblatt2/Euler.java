7b5403af19f693afc30255529afd72eb1281d440
tthunig <thunig@vsp.tu-berlin.de>
2016-11-01 16:25:30 +0100

    add empty methods

--- src/aufgabenblatt2/Euler.java
+++ src/aufgabenblatt2/Euler.java
@@ -2,9 +2,12 @@ package aufgabenblatt2;
 
 public class Euler {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public static double myExp(int x) {
+		// TODO berechne e^x mittels der auf Aufgabenblatt 2 angegebenen Formel
+	}
+	
+	public static double myExpFast(int x){
+		// TODO beschleunige myExp()
 	}
 
 }
