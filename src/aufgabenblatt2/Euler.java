17fa2249cce3ae4afb9e2d78d7d0efa6ae2ffe27
Theresa Thunig <thunig@vsp.tu-berlin.de>
2016-11-14 14:58:45 +0100

    return 0 to resolve compilation errors

--- src/aufgabenblatt2/Euler.java
+++ src/aufgabenblatt2/Euler.java
@@ -4,10 +4,12 @@ public class Euler {
 
 	public static double myExp(int x) {
 		// TODO berechne e^x mittels der auf Aufgabenblatt 2 angegebenen Formel
+		return 0;
 	}
 	
 	public static double myExpFast(int x){
 		// TODO beschleunige myExp()
+		return 0;
 	}
 
 }
